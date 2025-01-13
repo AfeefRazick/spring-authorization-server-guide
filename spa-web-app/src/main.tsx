cdaea4ab029e5b4dcdcc4d85b1178ebca1e9a079
Afeef Razick <afeefrazickamir@gmail.com>
2025-01-13 15:54:32 +0530

    Add oidc configuration and auth provider

--- spa-web-app/src/main.tsx
+++ spa-web-app/src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { UserManagerSettings } from "oidc-client-ts";
+import { AuthProvider } from "react-oidc-context";
+
+const CLIENT_ID = "public-client";
+const AUTH_SERVER_URL = "http://authserver:9000";
+const HOST_URL = window.location.origin;
+
+const oidcConfig: UserManagerSettings = {
+  authority: AUTH_SERVER_URL,
+  client_id: CLIENT_ID,
+  redirect_uri: HOST_URL + "/callback",
+  response_type: "code",
+  scope: "openid profile email",
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...oidcConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 );
